6125dda159221f5d965481a0c325e9a78fc7c334
liushengming <lsmya2018>
2021-04-26 14:14:28 +0800

    沉浸式状态栏修改按钮错误问题

--- common/src/main/java/cn/lsmya/common/widget/CompatToolbar.java
+++ common/src/main/java/cn/lsmya/common/widget/CompatToolbar.java
@@ -218,7 +218,7 @@ public class CompatToolbar extends RelativeLayout {
     public void hideNavigationIcon() {
         if (this.toolbar != null) {
             this.toolbar.setNavigationIcon(null);
-            this.navigationIconView.setVisibility(INVISIBLE);
+            this.navigationIconView.setVisibility(GONE);
         }
     }
 
