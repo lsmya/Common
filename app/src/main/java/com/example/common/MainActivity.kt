510875aa0fbe7ef78e784caad2f6b89a1233ea82
liushengming <lsmya2018>
2021-06-16 15:23:43 +0800

    init

--- app/src/main/java/com/example/common/MainActivity.kt
+++ app/src/main/java/com/example/common/MainActivity.kt
@@ -1,34 +1,12 @@
 package com.example.common
 
-import android.content.Intent
 import android.os.Bundle
-import cn.lsmya.common.base.SimpleActivity
-import cn.lsmya.common.empty.SysErrModel
-import cn.lsmya.common.extension.toast
-import cn.lsmya.common.utils.loge
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : SimpleActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.rootView,TestFragment()).commit()
     }
 
-    override fun onEventListener(sysErrModel: SysErrModel) {
-        when (sysErrModel.code) {
-            else -> {
-                sysErrModel.sysInfo?.let {
-                    toast(it)
-                }
-
-            }
-
-        }
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        loge("+++++++!!!!!!!!!!!!!!!")
-    }
 }
